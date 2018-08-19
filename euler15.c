3f683e92b326f4caf79c483133432f2196a5c8cc
Alieo <18249236296@163.com>
2018-08-19 23:54:41 +0800

    求解C40 20

---
+++ euler15.c
@@ -0,0 +1,37 @@
+/*************************************************************************
+> File Name: euler15.c
+> Author: 
+> Mail: 
+> Created Time: 2018年08月19日 星期日 23时42分44秒
+************************************************************************/
+/*
+*Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+
+How many such routes are there through a 20×20 grid?
+* */
+#include <stdio.h>
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+
+}
+int jc(int n) {
+    if (n == 1) return 1;
+    return n * jc(n - 1);
+
+}
+int main() {
+    long long int sum = 0;
+    long long int x = 1, y = 1;
+    for (int i = 39; i >= 21; i -= 2) {
+        x *= (2 * i);    
+
+    }
+    sum = (x / gcd(x, y)) / (jc(10) / gcd(x, y));
+    printf("%lld\n", sum);
+    return 0;
+
+}
+/*转换思维
+ *本题主要求解C40 20
+ * */
