f58f219ec1e690cdcecf1d7a4975cb87f468531c
Alieo <18249236296@163.com>
2018-08-20 20:11:06 +0800

    方阵求解C40 20

--- euler15.c
+++ euler15.c
@@ -7,8 +7,11 @@
 /*
 *Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
-
 How many such routes are there through a 20×20 grid?
+从一个2×2方阵的左上角出发，只允许向右或向下移动，则恰好有6条通往右下角的路径。
+
+对于20×20方阵来说，这样的路径有多少条？
+
 * */
 #include <stdio.h>
 int gcd(int a, int b) {
